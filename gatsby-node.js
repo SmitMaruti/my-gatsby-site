95b40f20b6510b5f4278b794cc1ae03d0859f696
SmitMaruti <smit.patel@marutitech.com>
2021-07-29 13:52:19 +0530

    slash redirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,5 +5,12 @@ exports.createPages = async ({ graphql, actions }) => {
 			toPath: '/about',
 			redirectInBrowser: true,
 			isPermanent: true,
-		});
+    });
+    createRedirect({
+			fromPath: '/blog/',
+			toPath: '/blog',
+			redirectInBrowser: true,
+			isPermanent: true,
+    });
+    
 }   
